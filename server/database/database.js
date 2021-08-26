a05f1dd747b6fba69f1ffde1ae7fda10a4d1680c
DimitarTanev1997 <dimitar.tanev1997@gmail.com>
2021-08-26 13:29:40 +0300

    Changes to database.js

--- server/database/database.js
+++ server/database/database.js
@@ -1,15 +1,21 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-let sequalize = new Sequelize(process.env.DATABASE_KEY);
-
-// if (process.env.NODE_ENV === 'production') {
-//     sequalize = new Sequelize(process.env.DATABASE_KEY);
-// } else {
-//     sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//         host: 'localhost',
-//         dialect: 'postgres'
-//     });
-// }
+if (process.env.NODE_ENV === 'production') {
+    sequalize = new Sequelize(process.env.DATABASE_KEY, {
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    });
+} else {
+    sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: 'localhost',
+        dialect: 'postgres'
+    });
+}
 
 module.exports = sequalize;
