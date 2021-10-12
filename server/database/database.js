b3a10029b19d96746be59186ecf9a9ece16c7055
DimitarTanev1997 <dimitar.tanev1997@gmail.com>
2021-10-12 10:59:52 +0300

    Remove locale db setup

--- server/database/database.js
+++ server/database/database.js
@@ -1,22 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-if (process.env.NODE_ENV === 'production') {
-    sequalize = new Sequelize(process.env.DATABASE_URL, {
-        logging: false,
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-        dialect: 'postgres'
-    });
-} else {
-    sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-        host: 'localhost',
-        dialect: 'postgres'
-    });
-}
+const sequalize = new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    dialect: 'postgres'
+});
 
 module.exports = sequalize;
